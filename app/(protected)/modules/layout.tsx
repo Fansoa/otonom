e72129484327f1a77f1ec299ab24846a97ae95ec
fansoa <fverin.contactpro@gmail.com>
2024-07-31 21:27:23 +0200

    refactor modules layout with new implementation

--- app/(protected)/modules/layout.tsx
+++ app/(protected)/modules/layout.tsx
@@ -2,10 +2,12 @@
 
 import Sidebar from "@/components/Sidebar/index.tsx";
 import { ReactNode } from "react";
-import useCurrentNavigation from "@/app/(protected)/modules/hooks/useCurrentNavigation/index.tsx";
 
 export default function Layout({ children }: { children: ReactNode }) {
-  const navigationWithCurrentProp = useCurrentNavigation();
-
-  return <Sidebar navigation={navigationWithCurrentProp}>{children}</Sidebar>;
+  return (
+    <>
+      <Sidebar />
+      <div className="lg:pl-72">{children}</div>
+    </>
+  );
 }
