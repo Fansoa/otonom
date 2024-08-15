13ff7052c39ab13391f7cdae40672e6e9916407c
fansoa <fverin.contactpro@gmail.com>
2024-08-15 15:10:41 +0200

    add somes services to communicate with db

---
+++ services/mealworm/index.ts
@@ -0,0 +1,27 @@
+export const getRackList = async ({ supabaseClient, userId }) => {
+  const response = await supabaseClient
+    .from("rack")
+    .select("*, crate(*)")
+    .eq("user_id", userId)
+    .abortSignal(AbortSignal.timeout(3000));
+
+  return response;
+};
+
+export const addRack = async ({ supabaseClient, user_id, name }) => {
+  const response = await supabaseClient
+    .from("rack")
+    .insert([{ name, user_id }])
+    .select();
+
+  return response;
+};
+
+export const addCrate = async ({ supabaseClient, rack_id, name }) => {
+  const response = await supabaseClient
+    .from("crate")
+    .insert([{ rack_id, name }])
+    .select();
+
+  return response;
+};
