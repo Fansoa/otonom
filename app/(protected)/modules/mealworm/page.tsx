5a0276c9002051d7127a031c0243c8dad18f8735
fansoa <fverin.contactpro@gmail.com>
2024-08-20 18:06:59 +0200

    import BreedingInterface with lazy loading

--- app/(protected)/modules/mealworm/page.tsx
+++ app/(protected)/modules/mealworm/page.tsx
@@ -1,12 +1,29 @@
 "use client";
 
+import dynamic from "next/dynamic";
+
 import useAuth from "@/hooks/useAuth/index.ts";
 import useRackList from "@/app/(protected)/modules/mealworm/hooks/useRackList/index.ts";
 
 import { RackListProvider } from "@/app/(protected)/modules/mealworm/contexts/RackListContext/index.tsx";
 import { SelectedItemAndSelectedItemDispatchProvider } from "@/app/(protected)/modules/mealworm/contexts/SelectedItemAndSelectedItemDispatchContext/index.tsx";
 
-import BreedingInterface from "@/app/(protected)/modules/mealworm/components/BreedingInterface/index.tsx";
+import Spinner from "@/components/Spinner/index.tsx";
+
+const BreedingInterface = dynamic(
+  () =>
+    import(
+      "@/app/(protected)/modules/mealworm/components/BreedingInterface/index.tsx"
+    ),
+  {
+    ssr: false,
+    loading: () => (
+      <div className="h-96 p-4 bg-slate-100 flex justify-center items-center">
+        <Spinner />
+      </div>
+    ),
+  },
+);
 
 const Mealworm = () => {
   const { supabaseClient, user } = useAuth();
