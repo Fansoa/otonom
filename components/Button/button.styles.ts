0fd34980cb8d34208a4ca132b29caa2910a96226
fansoa <fverin.contactpro@gmail.com>
2024-08-20 17:25:08 +0200

    add variant to button

---
+++ components/Button/button.styles.ts
@@ -0,0 +1,6 @@
+export const buttonStyles = {
+  primary:
+    " flex w-full justify-center rounded-md bg-green-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-green-500 border-2 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600",
+  secondary:
+    "flex w-full justify-center rounded-md px-3 py-1.5 text-sm font-semibold leading-6 text-green-600 shadow-sm hover:border-green-600 border-2 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600",
+};
--- components/Button/types.ts
+++ components/Button/types.ts
@@ -3,5 +3,6 @@ import { ComponentProps } from "react";
 export type ButtonProps = ComponentProps<"button"> & {
   className?: string;
   label: string;
+  variant?: "primary" | "secondary";
   onClick?: () => void;
 };
