8837bf50b39557692bd3781ad53d79dfc66e3180
fansoa <fverin.contactpro@gmail.com>
2024-08-22 17:52:51 +0200

    add ActionInterface

---
+++ app/(protected)/modules/mealworm/components/ActionInterface/index.tsx
@@ -0,0 +1,76 @@
+import { useRackListContext } from "@/app/(protected)/modules/mealworm/contexts/RackListContext/index.tsx";
+import { useSelectedItemContext } from "@/app/(protected)/modules/mealworm/contexts/SelectedItemAndSelectedItemDispatchContext/index.tsx";
+
+import TableList from "@/components/TableList/index.tsx";
+
+import {
+  getFormattedCrateActions,
+  getSelectedItemType,
+} from "@/app/(protected)/modules/mealworm/utils/methods/index.ts";
+
+const ActionInterface = () => {
+  const { rackList } = useRackListContext();
+
+  const selectedItem = useSelectedItemContext();
+
+  const selectedItemType = getSelectedItemType(selectedItem);
+
+  if (selectedItem) {
+    const rack = rackList.find((rack) => rack.id === selectedItem.rackId);
+
+    if (selectedItemType === "rack") {
+      const formattedCrateActionsOfRack = rack.crate
+        ? rack.crate.map(
+            (crate) => crate.action && getFormattedCrateActions(crate),
+          )
+        : [[]];
+
+      const rackActionList = formattedCrateActionsOfRack.flat();
+
+      return (
+        <TableList
+          itemList={rackActionList}
+          title="Action list"
+          description={`A list of all actions carried out on crates of rack: ${rack.name}`}
+          messageIfItemListIsEmpty={`${rack.name} does not contain any actions`}
+        />
+      );
+    }
+
+    if (selectedItemType === "crate") {
+      const crate = rack.crate.find(
+        (crate) => crate.id === selectedItem.crateId,
+      );
+      const crateActionList = crate.action
+        ? getFormattedCrateActions(crate)
+        : [];
+      return (
+        <TableList
+          itemList={crateActionList}
+          title="Action list"
+          description={`A list of all actions carried out on the crate: ${crate.name}`}
+          messageIfItemListIsEmpty="The selected element does not contain any actions"
+        />
+      );
+    }
+  }
+
+  const actionList = rackList
+    ?.map((rack) =>
+      rack?.crate
+        .map((crate) => crate.action && getFormattedCrateActions(crate))
+        .flat(),
+    )
+    .flat();
+
+  return (
+    <TableList
+      itemList={actionList}
+      title="Action list"
+      description="A list of all actions carried out"
+      messageIfItemListIsEmpty="The selected element does not contain any actions"
+    />
+  );
+};
+
+export default ActionInterface;
